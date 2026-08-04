8bbcfac556cebab09cfb1f2346625a439006860e
Trisha <your@email.com>
2026-08-04 22:44:17 +0530

    Add Tailwind config

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -6,8 +6,8 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+</StrictMode>
 );
